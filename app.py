f841f1d0c4faca2b9677a69fed5f5114e47891a6
VoltronD <bladimedrano54@gmail.com>
2026-04-04 21:16:50 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Panel de control")
-st.write("¡Esta es mi primera página!")
+st.title("⚒️ Mi Herramienta de Geología")
+
+# Entrada de datos técnica
+st.subheader("Registro de Muestras")
+col1, col2 = st.columns(2)
+
+with col1:
+    tipo = st.selectbox("Tipo de Roca", ["Ignea", "Sedimentaria", "Metamórfica"])
+with col2:
+    ubicacion = st.text_input("Ubicación (Coordenadas)")
+
+if st.button("Registrar Hallazgo"):
+    st.success(f"¡Muestra de roca {tipo} registrada en {ubicacion}!")
+    st.balloons() # ¡Globos de celebración!
